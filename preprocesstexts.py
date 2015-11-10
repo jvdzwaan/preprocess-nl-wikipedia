aa1fb6cd411f883b6e7e258b16a1986cb03ef034
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-11-10 16:48:55 +0100

    Lemmatize articles
    Also, an article is stored on a single line, so multiple articles can be
    stored in the same file.

--- preprocesstexts.py
+++ preprocesstexts.py
@@ -6,39 +6,64 @@ import codecs
 import uuid
 import os
 import tarfile
+import string
+import time
 from pattern.nl import parsetree
 
 
-def articles(input_dir):
-    text_files = glob.glob('{}*.txt'.format(input_dir))
-    for txt_file in text_files:
-        with codecs.open(txt_file, 'rb', 'utf-8') as f:
-            text = f.read()
-            articles = text.split('\n\n[[')
-            for article in articles:
-                yield article
+def articles(txt_file):
+    with codecs.open(txt_file, 'rb', 'utf-8') as f:
+        text = f.read()
+        articles = text.split('\n\n[[')
+        for article in articles:
+            yield article
+
+
+def lemmatize(text):
+    r = parsetree(text,
+                  tokenize=True,     # Split punctuation marks from words?
+                  tags=True,         # Parse part-of-speech tags? (NN, JJ, ...)
+                  chunks=False,      # Parse chunks? (NP, VP, PNP, ...)
+                  relations=False,   # Parse chunk relations? (-SBJ, -OBJ, ...)
+                  lemmata=True,      # Parse lemmata? (ate => eat)
+                  encoding='utf-8',  # Input string encoding.
+                  tagset=None)       # Penn Treebank II (default) or UNIVERSAL.
+
+    words = []
+    for sentence in r:
+        for lemma in sentence.lemmata:
+            if not lemma in string.punctuation and not lemma == 'http:':
+                words.append(lemma)
+                #print lemma
+    return words
+
 
 input_dir = '/home/jvdzwaan/data/wikipedia-text-sample/'
 output_dir = '/home/jvdzwaan/data/wikipedia-articles/'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-for article in articles(input_dir):
-    article = article.replace('[[', '')
-    article = article.replace(']]', '')
+text_files = glob.glob('{}*.txt'.format(input_dir))
+for i, txt_file in enumerate(text_files):
+    print '{} ({} of {})'.format(txt_file, i+1, len(text_files)),
 
-    out_file = os.path.join(output_dir, str(uuid.uuid4()))
+    out_file = os.path.join(output_dir, os.path.basename(txt_file))
+    start = time.time()
     with codecs.open(out_file, 'wb', 'utf-8') as f:
-        f.write(article)
+        for article in articles(txt_file):
+            article = article.replace('[', '')
+            article = article.replace(']', '')
+            article = article.replace('=', ' ')
+            article = article.replace('*', ' ')
+            article = article.replace('\'\'', ' ')
+            article = article.replace('"', ' ')
+            article = article.replace('|', ' ')
+            article = article.replace('\n', ' ')
 
-    #txt = parsetree(article,
-    #                tokenize=True,     # Split punctuation marks from words?
-    #                tags=True,         # Parse part-of-speech tags? (NN, JJ, ...)
-    #                chunks=False,      # Parse chunks? (NP, VP, PNP, ...)
-    #                relations=False,   # Parse chunk relations? (-SBJ, -OBJ, ...)
-    #                lemmata=True,      # Parse lemmata? (ate => eat)
-    #                encoding='utf-8',  # Input string encoding.
-    #                tagset=None)       # Penn Treebank II (default) or UNIVERSAL.
+            words = lemmatize(article)
+            f.write(' '.join(words)+'\n')
+    end = time.time()
+    print ' -> {} sec'.format(end-start)
 
     #print repr(txt)
 
